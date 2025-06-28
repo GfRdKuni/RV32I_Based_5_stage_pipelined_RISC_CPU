8de6c2314979a5e5fb9cc3438ac24815f6d3eec4
rkGuo <125114151+GfRdKuni@users.noreply.github.com>
2025-06-28 16:57:56 +0800

    ip

---
+++ RISCV32ICPU.runs/bram_img1b_synth_1/bram_img1b_stub.v
@@ -0,0 +1,23 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+// Date        : Sat Jan  6 10:49:39 2024
+// Host        : LGORVKE running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ bram_img1b_stub.v
+// Design      : bram_img1b
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35tcpg236-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_3,Vivado 2019.1" *)
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(clka, ena, addra, douta)
+/* synthesis syn_black_box black_box_pad_pin="clka,ena,addra[11:0],douta[3:0]" */;
+  input clka;
+  input ena;
+  input [11:0]addra;
+  output [3:0]douta;
+endmodule
